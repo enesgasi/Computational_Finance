a02b1aaee796827899db13e3852f77972a8d6919
Enes Gaşi <enesgasi@hotmail.com>
2026-01-06 14:22:50 +0300

    added mahmut bağcı's solution

---
+++ Lecture15/mahmut.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+btceur = pd.read_csv("BTCEUR_price.csv", header=None)
+eurusdt = pd.read_csv("EURUSDT_price.csv", header=None)
+btcusdt = pd.read_csv("BTCUSDT_price.csv", header=None)
+
+capital = 1 #btc
+
+margin = 1.0004
+
+for i in range(1000):
+    x = btceur.iloc[0,i]
+    y = btcusdt.iloc[0,i]
+    z = eurusdt.iloc[0,i]
+    ratio = (x * z) / y
+
+    if ratio > margin:
+        print(f"Arbitrage opportunity at index {i}:")
+        capital = float(capital)*ratio
+        print(f"New capital: {capital} BTC")
+
+percent = 100*capital-100        
+print("profit percentage:", percent)
